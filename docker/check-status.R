00c438e393c9c7fd32d1a31126f67d98da38a5bd
Diego H <diego.hernangomezherrero@gmail.com>
2023-08-12 21:11:24 +0200

    Add support for additional issues

--- docker/check-status.R
+++ docker/check-status.R
@@ -1,6 +1,8 @@
 # Modified version from pharmaverse/admiralci
 # See https://github.com/pharmaverse/admiralci/tree/61347fe11955297818b3ca7814fc7328f2ad7840/.github/actions/cran-status-extract
 
+
+# 0. Setup ----
 if (!requireNamespace("optparse", quietly = TRUE)) {
   suppressMessages(install.packages("optparse",
     repos = "https://cloud.r-project.org",
@@ -8,14 +10,14 @@ if (!requireNamespace("optparse", quietly = TRUE)) {
     quiet = TRUE
   ))
 }
-
-if (file.exists("cran-status.md")) unlink("cran-status.md")
-if (file.exists("issue.md")) unlink("issue.md")
-
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(rvest))
 suppressPackageStartupMessages(library(stringr))
 suppressPackageStartupMessages(library(digest))
+
+# clean files
+if (file.exists("cran-status.md")) unlink("cran-status.md")
+if (file.exists("issue.md")) unlink("issue.md")
 
 # check if needed : package name and working dir path as input arguments :
 library(optparse)
@@ -27,72 +29,93 @@ option_list <- list(
   ),
   make_option(c("-s", "--statuses"),
     type = "character", default = "ERROR,WARN,NOTE",
-    help = "status types (comma separated list, for exemple ERROR,WARN,NOTE",
+    help = "status types (comma separated list, e.g. ERROR,WARN,NOTE",
     metavar = "character"
   )
 )
-
 opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 
-parse_errors <- function(url) {
-  return(
-    tryCatch(url %>% read_html() %>% html_text(), error = function(e) "URL Not Found")
-  )
-}
-
-build_md5_codes <- function(pkg, errors, step) {
-  # Build current step links
-  errors[[sprintf("%sLinks", step)]] <-
-    str_c(
-      "https://www.r-project.org/nosvn/R.check/",
-      errors$Flavor, "/", pkg, sprintf("-00%s.html", tolower(step))
-    )
-
-
-  # Get details as text for current step
-  errors[[sprintf("%sDetails", step)]] <- lapply(errors[[sprintf("%sLinks", step)]],
-    FUN = parse_errors
-  )
-
-  # Create md5 codes for each step
-  errors[[sprintf("%sId", step)]] <- lapply(
-    errors[[sprintf("%sDetails", step)]],
-    digest
-  )
-
-  # Unlist results
-  errors[[sprintf("%sId", step)]] <- unlist(errors[[sprintf("%sId", step)]])
-
-  return(errors)
-}
-
+# 1. Fun helpers ----
 cran_status <- function(x) {
   cat(x, file = "cran-status.md", append = TRUE, sep = "\n")
 }
 
+# 2. Get Inputs ----
 pkg <- opt$package # paste(desc::desc_get(keys = "Package"))
-
-cat("Testing package:", pkg, "\n-----\n")
+# Get input status
+status_types <- opt$statuses
+statuses <- unlist(strsplit(status_types, split = ","))
 
 url <- sprintf("https://cran.r-project.org/web/checks/check_results_%s.html", pkg)
 
-if (!httr::http_error(url)) {
-  # Get input status
-  status_types <- opt$statuses
-  statuses <- unlist(strsplit(status_types, split = ","))
+# 3. Tests -----
+cat("Testing package:", pkg, "\n-----\n")
+## a. Url not reachable ----
 
-  # Parse html table into dataframe
-  checks <- url %>%
-    read_html() %>%
+if (httr::http_error(url)) {
+  cat(
+    paste0("::warning::Package ", pkg, " not found on CRAN.")
+  )
+
+  cran_status(
+    paste0(":x: **Package ", pkg, " not found on CRAN**:\n\n")
+  )
+
+  cran_status(paste0("Error accessing url:\n", url))
+
+  # Copy to issue
+  file.copy("cran-status.md", "issue.md")
+}
+
+## b. Found on CRAN, check issues ----
+
+if (!file.exists("cran-status.md")) {
+  scrap <- url %>%
+    read_html()
+
+  cranchecks <- scrap %>%
     html_element("table") %>%
     html_table()
 
-  # filter statuses and get their details links (and convert it to md5 unique code)
-  errors <- filter(checks, Status %in% statuses)
+  any_error <- any(cranchecks$Status %in% statuses)
+  additional <- grepl("Additional", html_text(scrap), ignore.case = TRUE)
 
-  # If errors table is empty: just get out !
-  if (dim(errors)[1] == 0) {
+  ### i. Has issues ----
+  if (any_error) {
+    cran_status(sprintf(
+      ":x: **CRAN checks for %s resulted in one or more (`%s`)s**:\n\n",
+      pkg,
+      status_types
+    ))
+    cran_status("\nSee the table below for a summary of the checks run by CRAN:\n\n")
+    cran_status(knitr::kable(cranchecks))
+    cran_status(sprintf(
+      "\n\nAll details and logs are available here: %s\n\n", url
+    ))
+    cat("::warning::One or more CRAN checks resulted in an invalid status\n")
+  }
+
+  ### ii. Has additional issues ----
+
+  if (additional) {
+    cran_status(sprintf(
+      ":x: **%s has `Additional Issues`**:\n",
+      pkg
+    ))
+    cran_status(sprintf(
+      "\nCheck the url: %s\n", url
+    ))
+    cat(paste0("::warning::", pkg, " has Additional issues\n"))
+  }
+
+  ## So far if cran-status.md exists, generate issue
+  if (file.exists("cran-status.md")) file.copy("cran-status.md", "issue.md")
+
+  ### iii. All OK ----
+  # If no issue has been generated
+
+  if (!file.exists("cran-status.md")) {
     cat(
       sprintf("None of this status found in the CRAN table. (status=%s)", status_types)
     )
@@ -104,53 +127,9 @@ if (!httr::http_error(url)) {
     ))
 
     cran_status("\nSee the table below for a summary of the checks run by CRAN:\n\n")
-    cran_status(knitr::kable(checks))
+    cran_status(knitr::kable(cranchecks))
     cran_status(sprintf(
       "\n\nAll details and logs are available here: %s", url
     ))
-  } else {
-    # Build each step md5 code
-    errors <- build_md5_codes(pkg, errors, "Build")
-    errors <- build_md5_codes(pkg, errors, "Check")
-    errors <- build_md5_codes(pkg, errors, "Install")
-
-    # Alphanumeric order on Flavor (for cran status comparison)
-    errors <- errors[order(errors$Flavor), ]
-
-    # Save into CSV:
-    errors %>%
-      select(Flavor, CheckId, InstallId, BuildId) %>%
-      write.csv("cran_errors.csv", row.names = FALSE)
-    if (any(checks$Status %in% statuses)) {
-      cran_status(sprintf(
-        ":x: **CRAN checks for %s resulted in one or more (`%s`)s**:\n\n",
-        pkg,
-        status_types
-      ))
-      cran_status("\nSee the table below for a summary of the checks run by CRAN:\n\n")
-      cran_status(knitr::kable(checks))
-      cran_status(sprintf(
-        "\n\nAll details and logs are available here: %s", url
-      ))
-
-      # Copy
-      file.copy("cran-status.md", "issue.md")
-
-      cat("::warning::One or more CRAN checks resulted in an invalid status\n")
-    }
   }
-} else {
-  cat(paste0(
-    "::warning::Package ", pkg,
-    " not found on CRAN."
-  ))
-
-  cran_status(paste0(
-    ":x: **Package ", pkg,
-    " not found on CRAN**:\n\n"
-  ))
-  cran_status(paste0("Error accessing url:\n", url))
-
-  # Copy
-  file.copy("cran-status.md", "issue.md")
 }
